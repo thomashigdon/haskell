41cf051239c2ea6fb25220dc41e7b16b5c674c60
Thomas Higdon <thomas.p.higdon@gmail.com>
2014-04-27 16:55:41 -0400

    more functions

--- exercises/baby.hs
+++ exercises/baby.hs
@@ -1,1 +1,31 @@
 doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                         then x
+                         else doubleMe x
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+fib :: (Integral a) => a -> a
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
--- parser/parser.hs
+++ parser/parser.hs
@@ -1,16 +1,55 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+                 char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+               first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom
+parseNumber :: Parser Integer
+-- parseNumber = liftM (Number . read)  (many1 (digit))
+--parseNumber = do
+--                 x <- many digit
+--                 return $ Number (read x)
+--parseNumber = many digit =<< read
+parseNumber = read =<< many digit
+--parseNumber = Number =<< read =<< many digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+
+--        <|> parseNumber
 
 main :: IO ()
 main = do
